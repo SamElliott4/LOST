32a4b897de10d096252b520837bf8f01d3fbb10b
Sam <SamElliott4@github.com>
2017-02-06 10:32:10 +0000

    Updated app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, render_template, flash, redirect, session
+from flask import Flask, url_for, request, render_template, redirect, session
 import psycopg2
 app = Flask(__name__)
 app.secret_key = 'flimsy key'
