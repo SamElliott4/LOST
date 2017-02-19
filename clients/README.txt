4033b284f9ef14aff2e97e2256f6ef57b39adf06
Sam <SamElliott4@github.com>
2017-02-19 18:35:26 +0000

    Clearing old files

--- clients/README.txt
+++
@@ -1,3 +0,0 @@
-This directory contains clients that interact with the LOST web service.
-
-For more information on usage of a client, call it without arguments
--- sql/import_data.sh
+++
@@ -1,8 +0,0 @@
-
-psql $1 -f create_tables.sql
-
-curl https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz | tar xvz
-
-python3 import_data.py $1 $2
-
-
